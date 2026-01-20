6342eefcfae6fcbdbff147f215f96008b4dc0774
Ralo <ralogu83@gmail.com>
2026-01-20 13:43:30 +0100

    Feature add client

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class travel {
     };
 };
 
-class fligth {
+class fligth extends travel{
     constructor(code, destination, price, airline, duration) {
         super(code, destination, price);
         this.airline = airline;
@@ -22,7 +22,7 @@ class fligth {
     };
 };
 
-class hotel {
+class hotel extends travel{
     constructor(code, destination, price, stars, roomType) {
         super(code, destination, price);
         this.stars = stars;
@@ -33,7 +33,7 @@ class hotel {
     };
 };
 
-class pack {
+class pack extends travel{
     constructor(code, destination, price, fligth, hotel) {
         super(code, destination, price);
         this.fligth = fligth;
@@ -67,13 +67,36 @@ class booking {
     };
 };
 
-let arrayCliente = [];
+let arrayClient = [];
+let arrayTravel = [];
+let arrayBooking = [];
 
 btnAddClient = document.getElementById("addClient");
 btnAddTravel = document.getElementById("addTravel");
 btnAddBooking = document.getElementById("addbooking");
+const tableClients = document.getElementById("tableClients");
 
-btnAddClient.addEventListener("click", addClient());
-function addClient(){
-    // Hay que hacer esta funcion y mirar  las opciones de los dropdowns
+btnAddClient.addEventListener("click", addClient);
+function addClient() {
+    const name = document.getElementById("nameInput").value;
+    const surname = document.getElementById("surnameInput").value;
+    const email = document.getElementById("emailInput").value;
+    const pNumber = document.getElementById("pNumberInput").value;
+
+    const newClient = new client(name, surname, email, pNumber);
+    arrayClient.push(newClient);
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${name}</td>
+        <td>${surname}</td>
+        <td>${email}</td>
+        <td>${pNumber}</td>
+        <td>
+            <button class="btn btn-danger btn-sm">Delete</button>
+        </td>
+    `;
+
+    tableClients.appendChild(row);
 }
